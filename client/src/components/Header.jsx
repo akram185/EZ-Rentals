53057cbe632dd22729fd02040be8cc4555e725a7
waseeme <wmohammed@leaders6-12.org>
2020-09-17 17:55:44 -0400

    user auth

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Header() {
+export default function Header(props) {
+  const { currentUser } = props
   return (
     <header>
       <h1>EZ Rentals</h1>
-      <Link to='/login'>Login/Register</Link>
+      {currentUser ? (
+        <div>
+          <p>{currentUser.username}</p>
+          <button onClick={props.handleLogout}>logout</button>
+        </div>
+      ) : (
+        <Link to='/login'>Login/Register</Link>
+      )}
       <hr />
+      {currentUser && (
+        <>
+          <Link to='/cars'>Cars</Link>
+          <Link to='/types'>Types</Link>
+        </>
+      )}
     </header>
   )
 }
