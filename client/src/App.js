9047d2182cf039375139c0b05869b8527617433d
waseeme <wmohammed@leaders6-12.org>
2020-09-17 13:58:00 -0400

    register form

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css'
+
 import Layout from './layouts/Layout'
 import Login from './screens/Login'
-import { loginUser } from './services/auth'
+import Register from './screens/Register'
+
+import { loginUser, registerUser } from './services/auth'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
 
   const loginSubmit = async (loginData) => {
     const userData = await loginUser(loginData)
+    setCurrentUser(userData)
+  }
+
+  const registerSubmit = async (registerData) => {
+    const userData = await registerUser(registerData)
     setCurrentUser(userData)
   }
 
@@ -23,7 +31,13 @@ function App() {
             loginSubmit={loginSubmit}
           />
         </Route>
-        
+
+        <Route path='/register'>
+          <Register
+            registerSubmit={registerSubmit}
+          />
+        </Route>
+
       </Switch>
     </Layout>
   )
